640db0ffb9d6a43b00ae1071188735b45ec6cd7d
Fabrizio Cannizzo <smartrics@gmail.com>
2011-05-09 23:45:49 +0100

    fiddled again with prog monitor

--- src/smartrics/ant/git/SimpleProgressMonitor.java
+++ src/smartrics/ant/git/SimpleProgressMonitor.java
@@ -5,9 +5,7 @@ import org.eclipse.jgit.lib.ProgressMonitor;
 public class SimpleProgressMonitor implements ProgressMonitor {
 
     private GitTask owner;
-    private int progress;
     private String name;
-    private int lastSz = -1;
 
     public SimpleProgressMonitor(GitTask t) {
         this.owner = t;
@@ -15,21 +13,11 @@ public class SimpleProgressMonitor implements ProgressMonitor {
 
     @Override
     public void update(int sz) {
-        if (lastSz == sz) {
-            return;
-        }
-        lastSz = sz;
-        int p = sz * 100 / progress;
-        if (p % 10 == 0) {
-            owner.log("  update: " + p);
-        }
-        progress = sz;
     }
 
     @Override
     public void start(int sz) {
-        progress = sz;
-        owner.log("  start: " + sz);
+        owner.log("[start] " + sz);
     }
 
     @Override
@@ -39,13 +27,13 @@ public class SimpleProgressMonitor implements ProgressMonitor {
 
     @Override
     public void endTask() {
-        owner.log("end: " + name);
+        owner.log("[end] " + name);
     }
 
     @Override
     public void beginTask(String what, int sz) {
         name = what;
-        owner.log("begin: " + what + " (" + sz + ")");
+        owner.log("[begin] " + what + " (" + sz + ")");
     }
 
 }
